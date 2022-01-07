be1074ac2495c53b3d2a1adfda670c98b494d039
John Hart <john@jh96.co.uk>
2022-01-07 13:27:22 +0000

    post fork changes

--- main.js
+++ main.js
@@ -1,141 +1,181 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module for mennu
-const Menu = electron.Menu
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+// Includes
+const electron = require('electron');
+const app = electron.app;
+const Menu = electron.Menu;
+const BrowserWindow = electron.BrowserWindow;
+const {shell} = require('electron')
 
-const path = require('path')
-const url = require('url')
+const httpPort = getRandomInt(30000);
 
-/////////////////////////////
 
-///////////////////////////////
-// Copy paste fixed by this 
-
+// Define App Menu
 app.on('ready', () => {
-//  createWindow() // commented for avoiding double window issue
-  if (process.platform === 'darwin') {
-    var template = [{
-      label: 'FromScratch',
-      submenu: [{
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click: function() { app.quit(); }
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'CmdOrCtrl+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+CmdOrCtrl+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'CmdOrCtrl+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'CmdOrCtrl+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'CmdOrCtrl+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'CmdOrCtrl+A',
-        selector: 'selectAll:'
-      }]
-    }];
-    var osxMenu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(osxMenu);
-  }
-})
-
-// PHP SERVER CREATION /////
-const PHPServer = require('php-server-manager');
-
-const server = new PHPServer({
-  
-    port: 5555,
-    directory: __dirname,
-    directives: {
-        display_errors: 1,
-        expose_php: 1
+  const template = [
+   {
+      label: "File",
+      submenu: [
+        {
+          label: "Print",
+          click: function() { print(); },
+          accelerator: 'CmdOrCtrl+P'
+        }
+      ]
+    },{
+      label: "Edit",
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          selector: 'undo:'
+        },{
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          selector: 'redo:'
+        },{
+          type: 'separator'
+        },{
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          selector: 'cut:'
+        },{
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:'
+        },{
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:'
+        },{
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:'
+        }
+      ]
+    },{
+      label: "View",
+        submenu: [
+          {
+            role: 'resetZoom'
+          },{
+            role: 'zoomIn'
+          },{
+            role: 'zoomOut'
+          },{
+            role: 'zoom'
+          }
+        ]
     }
+];
+if( process.env.DEBUG ) {
+  template.push({
+    label: 'Debugging',
+    submenu: [
+      {
+        label: 'Dev Tools',
+        role: 'toggleDevTools'
+      },
+
+      { type: 'separator' },
+      {
+        role: 'reload',
+        accelerator: 'Alt+R'
+      }
+    ]
+  });
+}
+function navigate( url ) {
+    mainWindow.loadURL('http://'+server.host+':'+server.port+'/'+url);
+}
+  var mainMenu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(mainMenu);
 });
 
-//////////////////////////
+// PHP Server
+const PHPServer = require('php-server-manager');
+var options;
+if( process.platform == "win32" ) {
+  options = {
+    port: httpPort,
+    host: "127.0.0.1",
+    directory: __dirname,
+    php: 'php/php.exe',
+    directives: {
+      display_errors: 1,
+      expose_php: 1
+    }
+  }
+} else {
+  options = {
+    port: httpPort,
+    host: "127.0.0.1",
+    directory: __dirname,
+    directives: {
+      display_errors: 1,
+      expose_php: 1
+    }
+  }
+}
+const server = new PHPServer(options);
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Main Window
 let mainWindow
-
-function createWindow () {
-
+function createWindow() {
   server.run();
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://'+server.host+':'+server.port+'/')
-
-/*
-mainWindow.loadURL(url.format({
-  pathname: path.join(__dirname, 'index.php'),
-  protocol: 'file:',
-  slashes: true
-}))
-*/
- const {shell} = require('electron')
- shell.showItemInFolder('fullPath')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      zoomFactor: 0.8,
+      enableRemoteModule: false,
+      scrollBounce: false
+    },
+    frame: true,
+    minWidth: 1024,
+    minHeight: 768
+  });
+  mainWindow.loadURL('http://'+server.host+':'+server.port+'/index.php');
+  shell.showItemInFolder('fullPath');
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    // PHP SERVER QUIT
     server.close();
+    app.quit();
     mainWindow = null;
-  })
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow) // <== this is extra so commented, enabling this can show 2 windows..
+// Init main window on win32
+app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// Window closed
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    // PHP SERVER QUIT
+  if( process.platform !== 'darwin' ) {
     server.close();
     app.quit();
   }
 })
 
+// Init main window on macOS
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
 
+// Disable security warnings
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
+// Functions
+function print() {
+  let win = BrowserWindow.getFocusedWindow();
+	// let win = BrowserWindow.getAllWindows()[0];
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+	win.webContents.print(options, (success, failureReason) => {
+		if (!success) console.log(failureReason);
+
+		console.log('Print Initiated');
+	});
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
